10fc64e452282cb2fbec4f9a459fd1a4c326aba6
Vedrowo <vedrangvozderac1@gmail.com>
2025-07-24 16:02:11 +0200

    working MPI, no implementation yet

--- src/main/java/org/example/DistributedMain.java
+++ src/main/java/org/example/DistributedMain.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import mpi.MPI;
+
 public class DistributedMain {
     public static void main(String[] args) throws Exception {
-        //MPI.Init()
+        MPI.Init(args);
+
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+
+        System.out.println("Hello from process "+ rank +" out of "+ size);
+
+        MPI.Finalize();
     }
 }
